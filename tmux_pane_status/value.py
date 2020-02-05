a59d3b4cf85df22c1e1133b05b8a41551dd7267f
yhiraki <coffexpr@gmail.com>
2020-02-05 17:22:40 +0900

    refactoring make_value

--- tmux_pane_status/value.py
+++ tmux_pane_status/value.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from functools import partial
 from . import directory, format, git
 
 
@@ -16,11 +17,20 @@ class LazyString:
 
 
 def make_values(*, cwd, options):
-    return {
-        'git_remote_server': LazyString(lambda: format.git_remote_server(git.remote(), options=options.get('git_remote_server'))),
-        'git_repository_name': LazyString(lambda: format.git_repository_name(git.remote(), options=options.get('git_repository_name'))),
-        'git_current_branch': LazyString(lambda: format.git_current_branch(git.branch_current(), options=options.get('git_current_branch'))),
-        'git_status_icons': LazyString(lambda: format.git_status_icons(git.status(), options=options.get('git_status_icons'))),
-        'cwd': LazyString(lambda: format.cwd(str(cwd), options=options['cwd'])),
-        'project_python': LazyString(lambda: format.project_python(directory.is_python(cwd), options=options.get('project_python')))
-    }
+    def f(fmt, c, opt=None):
+        if opt is None:
+            opt = {}
+        return fmt(c(), options=opt)
+    m = (
+        ('git_remote_server', git.remote),
+        ('git_repository_name', git.remote),
+        ('git_current_branch', git.branch_current),
+        ('git_status_icons', git.status),
+        ('cwd', partial(str, cwd)),
+        ('project_python', partial(directory.is_python, cwd)),
+    )
+    v = {}
+    for name, cmd in m:
+        formatter = getattr(format, name)
+        v[name] = LazyString(partial(f, formatter, cmd, opt=options.get(name)))
+    return v
