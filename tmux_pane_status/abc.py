7c945d1b81e9b510c49084077d1db99ab9b14009
name <email>
2020-02-14 10:04:53 +0900

    cache feature for formatter

--- tmux_pane_status/abc.py
+++ tmux_pane_status/abc.py
@@ -3,10 +3,12 @@
 
 
 class Formatter:
+    cache_enabled = False
 
-    def _set_options(self, s, options):
+    def _set_options(self, s, options, cache=None):
         if not options:
             return s
+        self.cache = cache
         opt = ''.join([f'#[{i}]' for i in options])
         end = '#[default]'
         return f'{opt}{s}{end}'
@@ -17,8 +19,20 @@ class Formatter:
     def _extract_data(self, *ss):
         raise NotImplementedError()
 
+    def _load_cache(self, c: dict):
+        raise NotImplementedError()
+
+    def _store_cache(self, c: dict):
+        raise NotImplementedError()
+
     def format(self, *ss, options, icons):
-        s = self._extract_data(*ss)
-        s = self._set_icons(s, icons)
-        s = self._set_options(s, options)
+        s = ''
+        if self.cache_enabled and self.cache:
+            s = self._load_cache()
+        else:
+            s = self._extract_data(*ss)
+            s = self._set_icons(s, icons)
+            s = self._set_options(s, options)
+        if self.cache_enabled:
+            self._store_cache()
         return s
